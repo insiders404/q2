676a177f2b9423438772ad5b01f2a4f767719f84
insiders404 <147330331+insiders404@users.noreply.github.com>
2023-10-13 06:15:04 +0530

    Add files via upload

---
+++ q2.php
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/css" href="styles.css"?>
+<books>
+    <book>
+        <title>Harry Potter and the Philosopher's Stone</title>
+        <category>Fantasy</category>
+        <price>19.99</price>
+        <quantity>5</quantity>
+    </book>
+    <book>
+        <title>To Kill a Mockingbird</title>
+        <category>Classic</category>
+        <price>12.99</price>
+        <quantity>3</quantity>
+    </book>
+    <book>
+        <title>The Da Vinci Code</title>
+        <category>Mystery</category>
+        <price>14.99</price>
+        <quantity>7</quantity>
+    </book>
+</books>
